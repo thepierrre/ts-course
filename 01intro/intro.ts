2a34820b2f9ff1955b305b998958ca2eb821642a
Piotr Owczarczyk <piotr@Piotrs-MacBook-Pro.local>
2024-03-08 13:07:40 +0000

    add 01intro

---
+++ 01intro/intro.ts
@@ -0,0 +1,6 @@
+let user = {name: "Piotr", age: 26};
+
+console.log("Piotr");
+console.log(user.name);
+
+// tsc intro.ts --> compile to js
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,9 @@
+let greeting: string = "Hello Piotr";
+
+let myNum = 6;
+// myNum = "6"; --> cannot assign a number to a string
+
+let userId: number = 3344555;
+let isLoggedIn: boolean = false;
+
+console.log(greeting);
