8929420bb71f36435614a29247cb217a22a8574d
Piotr Owczarczyk <piotr@Piotrs-MacBook-Pro.local>
2024-03-08 12:59:03 +0000

    add 02basics

---
+++ 02basics/myfunctions.ts
@@ -0,0 +1,25 @@
+function addTwo(num: number): number {
+    return num + 2;
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+
+}
+
+const loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+const getValue = (myVal: number): boolean | string => {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+
+const heroes = ["thor", "spiderman", "ironman"];
+
+heroes.map(hero => {
+    return `The hero is ${hero}`;
+})
+
+addTwo(5);
+loginUser("piotr", "piotr@gmail.com");
