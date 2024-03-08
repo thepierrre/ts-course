c483d879a599ad7a076c5a9601ba62d3a1d5ebd3
thepierrre <piotr.owczarczyk98@gmail.com>
2024-03-08 17:10:21 +0000

    add myArrays.ts

---
+++ 02basics/myArrays.ts
@@ -0,0 +1,21 @@
+const superheroes: string[] = [];
+// const superheroes: Array<string> = [] ==> same as above
+const heroPower: number[] = [];
+// const heroPower: Array<number> = [] ==> same as above
+
+type Person = {
+    name: string;
+    active: boolean;
+}
+
+const allUsers: Person[] = [];
+
+const MLModels: number[][] = [
+    [2, 5, 4, 3, 7],
+    [0, 2, 3],
+]
+
+
+superheroes.push("spiderman");
+heroPower.push(2);
+allUsers.push({name: "Peter", active: true});
